2dc91b1079383c3d6a7e0b5d8f3bc884e72613bd
suyog <mgrsuyog6@gmail.com>
2023-12-23 19:44:35 +0545

    deleted

---
+++ JSchool/src/School/Student.java
@@ -0,0 +1,21 @@
+package School;
+
+public class Student {
+	
+	private int id;
+	private String name;
+	private int grade;
+	private double feesPaid;
+	private double feesTotal;
+	
+	public Student(int id. String name, int grade) {
+		
+		this.id = id;
+		this.name = name;
+		this.grade = grade;
+		feesPaid = 0;
+		feesTotal = 100000;
+		
+	}
+	
+}
